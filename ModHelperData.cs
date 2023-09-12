687c75cf10e295ad301ae05d81ec2a614695bb99
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-09-12 14:51:51 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,12 @@
+namespace DartlingGunnerFourthPath;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "38.3";
+    public const string Version = "1.0.0";
+    public const string Name = "Dartling Gunner Fourth Path";
+    public const string Description = "This mod was made for tewtiy REQUIRE path plus plus";
+    public const string RepoOwner = "Mattcy1";
+    public const string RepoName = "Dartling Gunner Fourth Path";
+    public const string Icon = "DartlingGunnerPath-Portrait.png";
+}
